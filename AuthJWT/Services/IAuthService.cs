c81726e4364b2832bafef2f8d6f0428c7bed355f
Montaser <omarmontaser34@gmail.com>
2024-06-27 04:08:25 +0300

    Initial Commit

---
+++ AuthJWT/Services/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using AuthJWT.Models;
+
+namespace AuthJWT.Services
+{
+    public interface IAuthService
+    {
+        Task<AuthModel> RegisterAsync(RegisterModel model);
+        Task<AuthModel> GetTokenAsync(LogInModel model);
+        Task<string> AddRoleAsync(AddRoleModel model);
+    
+    }
+}
